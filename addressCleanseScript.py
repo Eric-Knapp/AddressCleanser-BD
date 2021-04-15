a99b1a2443fc5744de068b08b34ab875f2ae43af
Eric Knapp <73511389+E-Knapp1@users.noreply.github.com>
2021-04-15 14:46:07 -0400

    Add files via upload

---
+++ addressCleanseScript.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 16:44:05 2021
+
+@author: 10276976
+"""
+import pandas as pd
+import pandas_usaddress
+import os
+
+#load dataframe
+df = pd.read_excel(r'C:\Users\10276976\Desktop\Python\TestFile1.xlsx')
+
+
+#initiate usaddress
+df = pandas_usaddress.tag(df, ['address'], granularity='full', standardize=True)
+df['StreetNamePreDirectional'] = df['StreetNamePreDirectional'].str.upper() 
+df['StateName'] = df['StateName'].str.upper() 
+df["PlaceName"]= df["PlaceName"].str.upper().str.title() 
+df["StreetName"]= df["StreetName"].str.upper().str.title() 
+df["StreetNamePostDirectional"]= df["StreetNamePostDirectional"].str.upper().str.title() 
+df["OccupancyType"]= df["OccupancyType"].str.upper().str.title() 
+df["StreetNamePreType"]= df["StreetNamePreType"].str.upper().str.title() 
+df["StreetNamePostType"]= df["StreetNamePostType"].str.upper().str.title() 
+df["StreetNamePostDirectional"]= df["StreetNamePostDirectional"].str.upper()
+df["OccupancyType"]= df["OccupancyType"].str.upper().str.title() 
+df["Recipient"]= df["Recipient"].str.upper().str.title() 
+df["OccupancyIdentifier"]= df["OccupancyIdentifier"].str.upper().str.title() 
+df["SubaddressType"]= df["SubaddressType"].str.upper().str.title() 
+df['SubaddressIdentifier'] = df['SubaddressIdentifier'].str.upper()
+df['USPSBoxType'] = df['USPSBoxType'].str.upper()
+
+
+df['USPS Box'] = df['USPSBoxType']+ (' ' + df['USPSBoxID']).fillna('')
+
+df['Address 1'] = df['AddressNumber'] + (' ' + df['StreetNamePreDirectional']).fillna('')+ (' ' + df['StreetNamePreType']).fillna('')+ (' ' + df['StreetName']).fillna('')+ (' ' + df['StreetNamePostType']).fillna('')+ (' ' + df['StreetNamePostDirectional']).fillna('')
+
+df['Address 2a'] = df['OccupancyType'] + (' ' + df['OccupancyIdentifier']).fillna('')+ (' ' + df['SubaddressType']).fillna('')+ (' ' + df['SubaddressIdentifier']).fillna('')
+df['Address 2b'] = df['SubaddressType']+ (' ' + df['SubaddressIdentifier']).fillna('')
+df['Address 2'] = df['Address 2a'].fillna('')+ ('' + df['Address 2b']).fillna('')
+
+# df.drop(['AddressNumber','StreetName','StreetNamePreDirectional','StreetNamePostDirectional','StreetNamePrefix','StreetNameSuffix'], axis=1, inplace=True)
+df=df.rename(columns={"address": "Old Address", "PlaceName": "City", "StateName": "State","ZipCode": "Zip Code"})
+df = df[['SAP Number', 'GAG', 'name', 'Old Address', 'Address 1','Address 2','City','State','Zip Code','USPS Box']]
+with pd.ExcelWriter(r'C:\Users\10276976\Desktop\Python\TestOutput1.xlsx') as writer:  # doctest: +SKIP
+     df.to_excel(writer, sheet_name='new data',index=False)
+    
+
+os.startfile(r'C:\Users\10276976\Desktop\Python\TestOutput1.xlsx')
